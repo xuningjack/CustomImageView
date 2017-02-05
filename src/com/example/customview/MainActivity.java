bf3926424edfa42aa886cb6ce8707f3be36cff90
xuning108986@pwrd.com <xuning108986@pwrd.com>
2017-02-05 17:27:03 +0800

    first commit

---
+++ src/com/example/customview/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.customview;
+
+import com.example.customview05imageview.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
